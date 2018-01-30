3cf2ba84502fd12143e9dadd249d1a5e1a5c3648
luka <359593891@qq.com>
2018-01-30 15:44:14 +0800

    'luka'

---
+++ src/template/App.vue
@@ -0,0 +1,20 @@
+<template lang="jade">
+  #app
+    img(src="./assets/logo.png")
+    <HelloWorld></HelloWorld>
+    <span class="iconfont icon-hunningtujiaobanji"></span>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import './stylus/general.styl'
+
+export default {
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="stylus">
+</style>
