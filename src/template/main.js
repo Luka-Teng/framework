49b386a2ae5d678c02aebd4f50a7a58c26b9c3ad
luka <359593891@qq.com>
2018-03-26 11:11:50 +0800

    'luka'

--- src/template/main.js
+++ src/template/main.js
@@ -2,8 +2,8 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import store from '@/store'
-import router from '@/router'
+import store from './store'
+import router from './router'
 
 Vue.config.productionTip = false
 
